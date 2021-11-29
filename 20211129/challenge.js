9d87b6f3012d55798b62208af810200e75ac2fe4
Your Name <you@example.com>
2021-11-29 12:09:37 -0600

    Final

--- 20211129/challenge.js
+++ 20211129/challenge.js
@@ -5,10 +5,14 @@
 // example:  ret3x(1,2)(1,2)(3,4) = 19
 // example:  ret3x(1,2)(0,1)(0,5) = 10
 
-function ret3x(a1,b1){
-    return function (a2, b2){
-        return function (a3, b3){
-            return (a1*a2*a3)+(b1*b2*b3);
-        }
-    }
-}
+// function ret3x(a1,b1){
+//     return function (a2, b2){
+//         return function (a3, b3){
+//             return (a1*a2*a3)+(b1*b2*b3);
+//         }
+//     }
+// }
+
+let ret3x = (a1, b1) => (a2, b2) => (a3, b3) => (a1*a2*a3 + b1*b2*b3);
+
+console.log(ret3x(1,2)(3,4)(5,6));
